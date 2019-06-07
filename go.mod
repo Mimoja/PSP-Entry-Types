d46a4f81f3e805422f9bf954595c2fb98edcf78c
Mimoja <git@mimoja.de>
2019-06-07 11:06:52 +0200

    Add go parser

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Mimoja/PSP-Entry-Types
+
+go 1.12
---
+++ psp_entry_type.go
@@ -0,0 +1,54 @@
+package pspentrytype
+
+import (
+	"bufio"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+)
+
+
+type AMDPSPDirectoryEntryType struct {
+	Type    uint32
+	Name    string
+    ProposedName string
+	Comment string
+}
+
+
+func Types() []AMDPSPDirectoryEntryType {
+
+	_, filename, _, _ := runtime.Caller(0)
+
+	fpath := filepath.Dir(filename)
+	fpath = filepath.Join(fpath, "types.csv")
+
+    csvFile, _ := os.Open(fpath)
+    reader := csv.NewReader(bufio.NewReader(csvFile))
+    var entryTypes []AMDPSPDirectoryEntryType
+    for {
+        line, error := reader.Read()
+        if error == io.EOF {
+            break
+        } else if error != nil {
+            log.Fatal("Could not read csv: ", error)
+        }
+        id, _ := strconv.ParseInt(line[0], 0, 64)
+        entryTypes = append(entryTypes, AMDPSPDirectoryEntryType{
+            Type: uint32(id),
+            Name:  line[1],
+            ProposedName:  line[2],
+            Comment: line[3],
+        })
+    }
+    peopleJson, _ := json.Marshal(entryTypes)
+    fmt.Println(string(peopleJson))
+    return entryTypes
+}
+
